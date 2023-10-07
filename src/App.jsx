d49c8ae4bf3dad37dc383beb44056e16eefa3f73
Bayel Ibraev <bayelibraev1@gmail.com>
2023-10-08 01:31:02 +0600

    thesasaasda

--- src/App.jsx
+++ src/App.jsx
@@ -32,54 +32,22 @@ function App() {
             </h6>
           </div>
         </div>
-        {/* <img
-          src="/assets/DSC01869.JPG"
-          alt="none"
-          className="object-cover h-full absolute z-[-1]"
-        />
-        <div className="text-white h-full grid justify-center">
-          <div className="flex flex-col gap-2 items-center  mt-[80px]">
-            <h4 className="text-3xl text-yellow-400 font-semibold">
-              11.11.2023
-            </h4>
-            <h4 className="text-3xl font-semibold">Restoran "Bai Bol"</h4>
-          </div>
-        </div> */}
+    
       </div>
 
       <div className="h-[920px]">
-        {/* <img
-          src="/assets/DSC01786.JPG"
-          alt="none"
-          className={`object-cover h-full absolute z-[-1] ${
-            fade ? "blur-0" : "blur-sm"
-          } delay-700 duration-700`}
-        />
-        <div className="text-white h-full grid justify-center">
-          <div
-            className={`${
-              fade ? "opacity-1" : "opacity-0"
-            } delay-1000 duration-1000`}
-          >
-            <h1 className="text-7xl font-bold mt-[80px] text-center h-max">
-              <span>Askar</span> <br />{" "}
-              <span className="text-yellow-600 ">&</span> Aidana
-            </h1>
-            <h6 className="text-xl text-center text-lime-100">
-              are getting married
-            </h6>
-          </div>
-        </div> */}
+       
         <img
           src="/assets/DSC01858.JPG"
           alt="none"
           className="object-cover h-[920px] absolute z-[-1]"
         />
         <div className="text-white h-full grid justify-center">
-          <div className="flex flex-col gap-2 items-center  mt-[80px]">
+          <div className="flex flex-col gap-1 items-center  mt-[80px]">
             <h4 className="text-3xl text-yellow-400 font-semibold">
-              11.11.2023
+              11.11.2023 
             </h4>
+            <h4 className="text-3xl font-semibold">Talas city</h4>
             <h4 className="text-3xl font-semibold">Restoran "Bai Bol"</h4>
           </div>
         </div>
