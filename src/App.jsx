bd7e39ba309a68f0770502c40e312bf400e3fe74
Bayel Ibraev <bayelibraev1@gmail.com>
2023-10-08 01:00:51 +0600

    thasdaasdasd

--- src/App.jsx
+++ src/App.jsx
@@ -18,14 +18,14 @@ function App() {
         />
         <div className="text-white h-full grid justify-center">
           <div className={`${fade ? 'opacity-1' :"opacity-0"} delay-1000 duration-1000`}>
-            <h1 className="text-7xl font-bold mt-[100px] text-center h-max">
+            <h1 className="text-7xl font-bold mt-[80px] text-center h-max">
               <span>Askar</span> <br /> <span className="text-yellow-600 ">&</span> Aidana
             </h1>
             <h6 className="text-xl text-center text-lime-100">are getting married</h6>
           </div>
         </div>
         <img
-          src="src/assets/DSC01869.JPG"
+          src="/assets/DSC01869.JPG"
           alt="none"
           className="object-cover h-full absolute z-[-1]"
         />
